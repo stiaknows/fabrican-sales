60e352f6bf06e6f6dbdd3ea65be2fe34c75c07be
stiaknows <sebastianalphonce672@gmail.com>
2022-04-20 11:57:01 +0300

    first commit

---
+++ graded.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<string.h>
+
+main ()
+{
+    double marks;
+    char [15]
+    printf("grade distribution/n");
+    printf("================================================================/n");
+    printf("enter marks/n")
+    scanf("%lf",&marks);
+}
